a84e48cd2db9b1d5bf4c94a960795c23d1f65363
Haobui0305 <haobui2512@gmail.com>
2024-12-06 09:12:56 +0700

    to PK

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+
+export const PagesRoutes: Routes = [
+  // {
+  //   path: '',
+   
+  //   data: {
+  //     title: 'Starter',
+  //     urls: [
+  //       { title: 'Dashboard', url: '/dashboard' },
+  //       { title: 'Starter' },
+  //     ],
+  //   },
+  // },
+];
